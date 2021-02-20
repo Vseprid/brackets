d3e56b933405f394822442f44a44f08a1c56a3e1
Vseprid <vseprid@gmail.com>
2021-02-20 17:09:11 +0200

    init: begin task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const pipes = bracketsConfig.some(e => e.some(x => x === '|'));
+    if (pipes) {
+        bracketsConfig.reduce()
+    }
+    const o = bracketsConfig.reduce((acc, [open, close]) => ({ ...acc, [close]: open }), {})
+    const stack = []
+    for (let s of str) {
+        if (!o[s]) {
+            stack.push(s)
+        } else {
+            if (stack.pop() !== o[s]) return false
+        }
+    }
+    return stack.length === 0
 }
